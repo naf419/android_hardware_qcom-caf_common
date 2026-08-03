9897c9a3a6ba1105bb60e8404acb4c168024d669
naf <naf@sdf.org>
2026-08-03 13:10:00 -0500

    common: split sm6225 4.19 and 5.15 HALs for bengal/khaje

--- qcom_defs.mk
+++ qcom_defs.mk
@@ -14,6 +14,12 @@ UM_5_15_FAMILY := kalama crow
 UM_6_1_FAMILY := pineapple volcano
 UM_6_6_FAMILY := sun
 
+ifeq ($(TARGET_KERNEL_VERSION),5.15)
+# UM 4.19 upgraded to UM 5.15
+UM_5_15_FAMILY := $(UM_5_15_FAMILY) $(UM_4_19_FAMILY)
+UM_4_19_FAMILY :=
+endif
+
 ifeq ($(TARGET_KERNEL_VERSION),6.6)
 # UM 5.10 upgraded to UM 6.6
 UM_6_6_FAMILY := $(UM_6_6_FAMILY) $(UM_5_10_FAMILY)
